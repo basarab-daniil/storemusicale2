5e06aa55c621f21a3832544a50df262248a3b0b0
basarab-daniil <basarab.daniil@iisgalvanimi.edu.it>
2026-05-28 07:48:08 +0000

    aggiunta file, componenti

---
+++ Frontend/src/app/playlists/playlists.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlists',
+  imports: [],
+  templateUrl: './playlists.html',
+  styleUrl: './playlists.css',
+})
+export class Playlists {}
---
+++ Frontend/src/app/tracks/tracks.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tracks',
+  imports: [],
+  templateUrl: './tracks.html',
+  styleUrl: './tracks.css',
+})
+export class Tracks {}
